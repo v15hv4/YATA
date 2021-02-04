ace5ba2ead5898c7122441e665f2ba83df008774
v15hv4 <vishva2912@gmail.com>
2021-02-04 19:42:01 +0530

    New TodoItem structure

--- lib/views/widgets/todo_list.dart
+++ lib/views/widgets/todo_list.dart
@@ -10,28 +10,28 @@ class _TodoListState extends State<TodoList> {
   @override
   Widget build(BuildContext context) {
     final _todoItems = [
-      {"title": "One"},
-      {"title": "Two"},
-      {"title": "Three"},
-      {"title": "Four"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
-      {"title": "Five"},
+      {"title": "One", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Two", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Three", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Four", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Five", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Six", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Seven", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Eight", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Nine", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Ten", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Eleven", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Twelve", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Thirteen", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Fourteen", "completed": false, "timestamp": DateTime.now()},
+      {"title": "Fifteen", "completed": false, "timestamp": DateTime.now()},
     ];
 
     return ListView.builder(
       itemCount: _todoItems.length + 1,
       itemBuilder: (context, index) {
         if (index == _todoItems.length) {
-          return SizedBox(height: 100.0);
+          return SizedBox(height: 50.0);
         }
         return TodoItem(_todoItems[index]);
       },
